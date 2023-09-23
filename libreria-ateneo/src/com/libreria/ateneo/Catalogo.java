2cee5c1b0be95966fbdb7c55969557065c2656a4
Gabriel <gab.serrano@duocuc.cl>
2023-09-23 20:42:10 -0300

    Agrega funcionalidades 2

--- libreria-ateneo/src/com/libreria/ateneo/Catalogo.java
+++ libreria-ateneo/src/com/libreria/ateneo/Catalogo.java
@@ -1,9 +1,47 @@
 package com.libreria.ateneo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Gabriel
  */
 public class Catalogo {
-        
+    private ArrayList<Libro> catalogo;
+
+    public Catalogo() {
+        this.catalogo = new ArrayList<>();
+    }
+    
+    //Agregar libros
+    public void agregaLibros(Libro libro){
+        this.catalogo.add(libro);
+    }
+    
+    //Mostrar libros
+    public void mostrarCatalogo(){
+        for (Libro libro : catalogo) {
+            libro.imprimirInfo();
+        }
+    }
+    
+    //Eliminar libros
+    public void eliminarLibroIsbn (String isbn){
+        for (Libro libro : catalogo) {
+            if (libro.getIsbn().equalsIgnoreCase(isbn)){
+                catalogo.remove(libro);
+                break;
+            }          
+        }
+    }
+    
+    //buscar por apellido autor
+    public void buscarLibroApellido (String apellido){
+        for (Libro libro : catalogo) {
+            if (libro.getAutor().getApellido().equalsIgnoreCase(apellido)){
+                libro.imprimirInfo();
+                break;
+            }
+        }
+    }
 }
