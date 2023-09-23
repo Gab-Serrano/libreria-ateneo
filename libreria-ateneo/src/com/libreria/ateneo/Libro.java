34f692834ecde177c8bae509ed38195e6e4d16f7
Gabriel <gab.serrano@duocuc.cl>
2023-09-23 19:08:50 -0300

    Se crea clase libro y atributos

---
+++ libreria-ateneo/src/com/libreria/ateneo/Libro.java
@@ -0,0 +1,14 @@
+package com.libreria.ateneo;
+
+/**
+ *
+ * @author Gabriel Serrano
+ * @version 
+ */
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int precio;
+    private String isbn;
+        
+}
